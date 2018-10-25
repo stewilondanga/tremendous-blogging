a590b00ce8a3e254454f7ccedb155e76b441e7c3
Stewart ilondanga <stilondanga@gmail.com>
2018-10-25 18:16:13 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,30 +12,30 @@ var navigate = (function() {
         let elems = document.querySelectorAll('p, li');
 
         elems.forEach(elem => {
+
+              // The result string will be tacked on to this
+              var result = '';
               /*
-                    	        // The result string will be tacked on to this
-                    	        var result = '';
+                                  	        // Split words/tags into array
+                                  	        let textItems = elem.innerHTML.trim().replace(/&nbsp;/g, ' ').split(/ (?=[^>]*(?:<|$))/);
 
-                    	        // Split words/tags into array
-                    	        let textItems = elem.innerHTML.trim().replace(/&nbsp;/g, ' ').split(/ (?=[^>]*(?:<|$))/);
+                                  	        // Find the second to last work
+                                  	        var targetWord = textItems[(textItems.length - 2)];
 
-                    	        // Find the second to last work
-                    	        var targetWord = textItems[(textItems.length - 2)];
+                                  	        // Stick a no break space to the end of the word and replace the instance in the array
+                                  	        textItems[(textItems.length - 2)] = targetWord + '&nbsp;';
 
-                    	        // Stick a no break space to the end of the word and replace the instance in the array
-                    	        textItems[(textItems.length - 2)] = targetWord + '&nbsp;';
+                                  	        // Join the words back together
+                                  	        result = textItems.join(' ');
 
-                    	        // Join the words back together
-                    	        result = textItems.join(' ');
+                                  	        // Replace whitespace after no break spaces
+                                  	        result = result.replace(/&nbsp; /g, '&nbsp;');
 
-                    	        // Replace whitespace after no break spaces
-                    	        result = result.replace(/&nbsp; /g, '&nbsp;');
-
-                    	        // Set the content of the element with our shiny string
-                    	        elem.innerHTML = result;
-                    	    });
-                    	}
+                                  	        // Set the content of the element with our shiny string
+                                  	        elem.innerHTML = result;
+                                  	    });
+                                  	}
 
 
-                    	// Run the things!
-                    	noOrphansPlz();
+                                  	// Run the things!
+                                  	noOrphansPlz();
