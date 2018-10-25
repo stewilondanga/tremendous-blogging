3ff3edc56b28c7aa7f859e2d5c23f590507359a0
Stewart ilondanga <stilondanga@gmail.com>
2018-10-25 18:15:39 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,37 @@ var navigate = (function() {
         $('.dd').hide();
         $('.' + dataName).toggle();
       });
-      /*
-      	// Prevent orphans
-      	function noOrphansPlz() {
 
-      	    let elems = document.querySelectorAll('p, li');
+      // Prevent orphans
+      function noOrphansPlz() {
 
-      	    elems.forEach(elem => {
+        let elems = document.querySelectorAll('p, li');
 
-      	        // The result string will be tacked on to this
-      	        var result = '';
+        elems.forEach(elem => {
+              /*
+                    	        // The result string will be tacked on to this
+                    	        var result = '';
 
-      	        // Split words/tags into array
-      	        let textItems = elem.innerHTML.trim().replace(/&nbsp;/g, ' ').split(/ (?=[^>]*(?:<|$))/);
+                    	        // Split words/tags into array
+                    	        let textItems = elem.innerHTML.trim().replace(/&nbsp;/g, ' ').split(/ (?=[^>]*(?:<|$))/);
 
-      	        // Find the second to last work
-      	        var targetWord = textItems[(textItems.length - 2)];
+                    	        // Find the second to last work
+                    	        var targetWord = textItems[(textItems.length - 2)];
 
-      	        // Stick a no break space to the end of the word and replace the instance in the array
-      	        textItems[(textItems.length - 2)] = targetWord + '&nbsp;';
+                    	        // Stick a no break space to the end of the word and replace the instance in the array
+                    	        textItems[(textItems.length - 2)] = targetWord + '&nbsp;';
 
-      	        // Join the words back together
-      	        result = textItems.join(' ');
+                    	        // Join the words back together
+                    	        result = textItems.join(' ');
 
-      	        // Replace whitespace after no break spaces
-      	        result = result.replace(/&nbsp; /g, '&nbsp;');
+                    	        // Replace whitespace after no break spaces
+                    	        result = result.replace(/&nbsp; /g, '&nbsp;');
 
-      	        // Set the content of the element with our shiny string
-      	        elem.innerHTML = result;
-      	    });
-      	}
+                    	        // Set the content of the element with our shiny string
+                    	        elem.innerHTML = result;
+                    	    });
+                    	}
 
 
-      	// Run the things!
-      	noOrphansPlz();
+                    	// Run the things!
+                    	noOrphansPlz();
